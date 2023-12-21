78d56342ded9dd1ece0a0f0f1bbda3876113d61d
Lu1815 <luisfelipece17@gmail.com>
2023-12-21 09:44:55 -0600

    Removed unnecesary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Home from './App/pages/Home';
-import ContactCard from './ui/ContactCard';
 
 function App() {
   return (
     <div className="App container mx-auto">
       <Home />
-      <ContactCard contactName='Jhon Doe' contactImage='https://example.com/avatar.jpg' />
     </div>
   );
 }
